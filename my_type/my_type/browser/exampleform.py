949a53be2f67a5122a4b4bd84863a4f3ee233af5
Alex Clark <aclark@aclark.net>
2014-02-02 17:00:08 -0500

    Fix error

--- my_type/my_type/browser/exampleform.py
+++ my_type/my_type/browser/exampleform.py
@@ -1,6 +1,6 @@
 from zope import interface, schema
 from zope.formlib import form
-from Products.Five.formlib import formbase
+#from Products.Five.formlib import formbase
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
 from my_type.my_type import my_typeMessageFactory as _
@@ -17,15 +17,15 @@ class IExampleFormSchema(interface.Interface):
         )
 
 
-class ExampleForm(formbase.PageForm):
-    form_fields = form.FormFields(IExampleFormSchema)
-    label = _(u'Example Form')
-    description = _(u'')
-
-    @form.action('Submit')
-    def actionSubmit(self, action, data):
-        pass
-        # Put the action handler code here 
+#class ExampleForm(formbase.PageForm):
+#    form_fields = form.FormFields(IExampleFormSchema)
+#    label = _(u'Example Form')
+#    description = _(u'')
+#
+#    @form.action('Submit')
+#    def actionSubmit(self, action, data):
+#        pass
+#        # Put the action handler code here 
 
 
 
